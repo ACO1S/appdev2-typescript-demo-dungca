62f1cb156e668acb991cfb0abf55394d56355db8
Francois Dungca <francoisdungca0521@gmail.com>
2026-03-27 15:47:32 +0800

    Done playing and updating the 05-enum.ts

--- 05-enum.ts
+++ 05-enum.ts
@@ -1,14 +1,33 @@
-(() => {
-    enum Role {
-        // SuperAdmin,
-        // SuperAdmin = 1,
-        SuperAdmin = 'SuperAdmin',
-        // Teacher,
-        Teacher = 'Teacher',
-        // Student,
-        Student = 'Student',
-    }
+enum Role {
+    SuperAdmin = 'SuperAdmin',
+    Teacher = 'Teacher',
+    Student = 'Student',
+}
 
-    // let userRole: Role = 0;
-    let userRole: Role = Role.SuperAdmin;
-})
+interface User {
+    name: string;
+    role: Role;
+}
+
+const users: User[] = [
+    { name: "Lovely", role: Role.SuperAdmin },
+    { name: "Marga", role: Role.Teacher },
+    { name: "Shobe", role: Role.Student },
+];
+
+function getDashboard(user: User): string {
+    switch (user.role) {
+        case Role.SuperAdmin:
+            return `${user.name} sees SuperAdmin Dashboard`;
+        case Role.Teacher:
+            return `${user.name} sees Teacher Dashboard`;
+        case Role.Student:
+            return `${user.name} sees Student Dashboard`;
+        default:
+            return `${user.name} has no access`;
+    }
+}
+
+for (const user of users) {
+    console.log(getDashboard(user));
+}
